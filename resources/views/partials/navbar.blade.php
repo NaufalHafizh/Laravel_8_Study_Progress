732ae6c00d94cb68be8064923cf8db9319496b42
Muhammad Naufal Hafizh <mnaufal201@gmai.com>
2021-08-15 10:02:58 +0700

    Update navbar.blade.php

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,10 +1,10 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+{{-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-      <a class="navbar-brand text-uppercase" href="#">Naufal</a>
+      <a class="float-md-start navbar-brand text-uppercase fw-bold" href="#">Naufal</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse float-end" id="navbarNavAltMarkup">
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
           <a class="nav-link {{ ($title === "HOME") ? 'active' : '' }}" href="/">Home</a>
           <a class="nav-link {{ ($title === "ABOUT") ? 'active' : '' }}" href="/about">About</a>
@@ -12,4 +12,27 @@
         </div>
       </div>
     </div>  
+  </nav> --}}
+
+  <nav class="navbar navbar-expand-lg shadow-sm navbar-light text-uppercase bg-light">
+    <div class="container">
+      <h3 class="float-md-start text-dark">Naufal.</h3>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link {{ ($title === "HOME") ? 'active' : '' }}" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link {{ ($title === "ABOUT") ? 'active' : '' }}" href="/about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link {{ ($title === "TULISAN") ? 'active' : '' }}" href="/tulisan">Tulisan</a>
+          </li>
+        </ul>
+      </div>
+    </div>
   </nav>
