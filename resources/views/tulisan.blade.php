6002aa1a6f3ead968ec2196727af648621719396
Muhammad Naufal Hafizh <mnaufal201@gmai.com>
2021-08-15 09:25:02 +0700

    Update tulisan.blade.php

--- resources/views/tulisan.blade.php
+++ resources/views/tulisan.blade.php
@@ -11,7 +11,7 @@
         <h2>
             <a class="text-decoration-none text-light" href="/tulisan/{{ $post->slug }}"> {{ $post->judul }}</a>
         </h2>
-        <p> <a class="text-decoration-none" href="/penulis/{{ $post->penulis->username }}">{{ $post->penulis->name  }}</a> dalam <a class="text-decoration-none" href="/kategori/{{ $post->kategori->slug }}"> {{ $post->kategori->nama}} </a> tanggal {{ $post->tanggal }} </p>
+        <p> <a class="text-decoration-none" href="/penulis/{{ $post->penulis->username }}">{{ $post->penulis->name  }}</a> dalam <a class="text-decoration-none" href="/kategori/{{ $post->kategori->slug }}"> {{ $post->kategori->nama}} </a> tanggal {{ $post["created_at"] }} </p>
         <p class="lead"> {!! $post->excerpt !!}</p>
         <a class="text-decoration-none" href="/tulisan/{{ $post->slug }}"> Read more...</a> 
     </article>
