754e495c81381aebc7a88f4f8b2386166d3ae36b
Muhammad Naufal Hafizh <56239834+NaufalHafizh@users.noreply.github.com>
2021-09-21 15:39:29 +0700

    Create sidebar.blade.php

---
+++ resources/views/dashboard/layout/sidebar.blade.php
@@ -0,0 +1,25 @@
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+    <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link {{ Request::is('dashboard') ? 'active' : '' }}" aria-current="page"
+                    href="/dashboard">
+                    <span data-feather="home"></span>
+                    Dashboard
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link {{ Request::is('dashboard/posts') ? 'active' : '' }}" href="/dashboard/posts">
+                    <span data-feather="file"></span>
+                    My Post
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link {{ Request::is('dashboard/posts/create') ? 'active' : '' }}"
+                    href="/dashboard/posts/create"><span data-feather="file-plus"></span>
+                    Create Post
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
