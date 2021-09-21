de9e495ac8c625d380b234ef8f3d17d79846ab4f
Muhammad Naufal Hafizh <56239834+NaufalHafizh@users.noreply.github.com>
2021-09-21 15:38:52 +0700

    Update database.php

--- app/Models/database.php
+++ app/Models/database.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Database extends Model
 {
     use HasFactory;
+    use Sluggable;
+
     // protected $fillable = ['judul', 'pengarang', 'excerpt', 'isi'];
     protected $guarded = ['id'];
     protected $with = ['penulis', 'kategori'];
@@ -46,5 +49,19 @@ class Database extends Model
     public function penulis()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function GetRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'judul'
+            ]
+        ];
     }
 }
