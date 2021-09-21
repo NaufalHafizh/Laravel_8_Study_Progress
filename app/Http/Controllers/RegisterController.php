eccd0f37162d2e6f360b32d3fc37e6c38ea457ad
Muhammad Naufal Hafizh <56239834+NaufalHafizh@users.noreply.github.com>
2021-09-21 15:38:49 +0700

    Update RegisterController.php

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -24,7 +24,7 @@ class RegisterController extends Controller
         $validatedData = $request->validate([
 
             'name' => 'required|max:255',
-            'username' => ['required', 'min:3', 'max:255', 'unique:users'],
+            'username' => 'required|min:3|max:255|unique:users',
             'email' => 'required|email:dns|unique:users',
             'password' => 'required|min:5|max:255'
         ]);
