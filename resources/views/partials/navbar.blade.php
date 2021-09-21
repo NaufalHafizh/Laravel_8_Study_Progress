abb5a1cb01242d6369c23f5e2e40ee6426d2f9ab
Muhammad Naufal Hafizh <56239834+NaufalHafizh@users.noreply.github.com>
2021-09-21 15:39:43 +0700

    Update navbar.blade.php

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,44 +1,47 @@
-{{-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-      <a class="float-md-start navbar-brand text-uppercase fw-bold" href="#">Naufal</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-link {{ ($title === "HOME") ? 'active' : '' }}" href="/">Home</a>
-          <a class="nav-link {{ ($title === "ABOUT") ? 'active' : '' }}" href="/about">About</a>
-          <a class="nav-link {{ ($title === "TULISAN") ? 'active' : '' }}" href="/tulisan">Tulisan</a>
-        </div>
-      </div>
-    </div>  
-  </nav> --}}
-
   <nav class="navbar navbar-expand-lg shadow-sm navbar-light text-uppercase bg-light">
-    <div class="container">
-      <h3 class="float-md-start text-dark">Naufal.</h3>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <a class="nav-link {{ ($title === "HOME") ? 'active' : '' }}" href="/">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link {{ ($title === "ABOUT") ? 'active' : '' }}" href="/about">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link {{ ($active === "TULISAN") ? 'active' : '' }}" href="/tulisan">Tulisan</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link {{ ($active === "KATEGORIS") ? 'active' : '' }}" href="/Kategoris">Kategori</a>
-          </li>
-          <li>
-            <a class="nav-link btn bg-primary {{ ($active === "Login") ? 'active' : '' }}" href="/login">Login</a>
-          </li>
-        </ul>
+      <div class="container">
+          {{-- <h3 class="float-md-start text-dark">Naufal.</h3> --}}
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+              aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                  <li class="nav-item">
+                      <a class="nav-link {{ $title === 'HOME' ? 'active' : '' }}" href="/">Home</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link {{ $title === 'ABOUT' ? 'active' : '' }}" href="/about">About</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link {{ $active === 'TULISAN' ? 'active' : '' }}" href="/tulisan">Tulisan</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link {{ $active === 'KATEGORIS' ? 'active' : '' }}" href="/Kategoris">Kategori</a>
+                  </li>
+              </ul>
+          </div>
+          <ul class="navbar-nav ms-auto">
+              @auth
+                  <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+                          aria-expanded="false">Selamat Datang, {{ auth()->user()->username }}</a>
+                      <ul class="dropdown-menu">
+                          <li>
+                              <form action="/logout" method="POST">
+                                  @csrf
+                                  <button type="submit" class="dropdown-item">LOGOUT</button>
+                              </form>
+                          </li>
+                          <li><a class="dropdown-item" href="/dashboard">Dashboard</a></li>
+                      </ul>
+                  </li>
+              @else
+                  <li class="nav-item">
+                      <a class="nav-link btn bg-primary {{ $active === 'Login' ? 'active' : '' }}" href="/login">Login</a>
+                  </li>
+              @endauth
+          </ul>
+
       </div>
-    </div>
   </nav>
