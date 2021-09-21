f52fcdcb3df9e3655a7400184d2909592c0cb7a6
Muhammad Naufal Hafizh <56239834+NaufalHafizh@users.noreply.github.com>
2021-09-21 15:38:47 +0700

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -14,5 +15,33 @@ class LoginController extends Controller
             'title' => 'Login',
             'active' => 'login'
         ]);
+    }
+
+    public function auth(Request $request)
+    {
+        $crendential = $request->validate([
+
+            'username' => 'required|min:3',
+            'password' => 'required|min:5'
+        ]);
+
+        if (Auth::attempt($crendential)) {
+
+            $request->session()->regenerate();
+
+            return redirect()->intended('/dashboard');
+        }
+
+        return back()->with('LoginGagal', 'Login Gagal');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/');
     }
 }
