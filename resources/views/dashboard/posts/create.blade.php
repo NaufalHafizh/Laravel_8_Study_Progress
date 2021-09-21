951267d55f893ac48bf123b781f67e117a1f624a
Muhammad Naufal Hafizh <56239834+NaufalHafizh@users.noreply.github.com>
2021-09-21 15:39:31 +0700

    Create create.blade.php

---
+++ resources/views/dashboard/posts/create.blade.php
@@ -0,0 +1,63 @@
+@extends('dashboard.layout.main')
+
+@section('container')
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Create Post</h1>
+    </div>
+    <div class="col-md-8">
+        <form method="post" action="/dashboard/posts">
+            @csrf
+            <div class="mb-3">
+                <label for="judul" class="form-label">Judul</label>
+                <input type="text" class="form-control @error('judul') is-invalid @enderror" id="judul" name="judul"
+                    value="{{ old('judul') }}">
+                @error('judul')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="slug" class="form-label">Slug</label>
+                <input type="text" class="form-control @error('slug') is-invalid @enderror" id="slug" name="slug" required
+                    value="{{ old('slug') }}" required readonly>
+                <div class="mb-3">
+                    <label for="kategori" class="form-label">Kategori</label>
+                    <select class="form-select" name="kategori_id">
+                        @foreach ($kategoris as $kategori)
+                            @if (old('kategori_id') == $kategori->id)
+                                <option value="{{ $kategori->id }}" selected>{{ $kategori->nama }}</option>
+                            @else
+                                <option value="{{ $kategori->id }}">{{ $kategori->nama }}</option>
+                            @endif
+
+                        @endforeach
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <input id="isi" type="hidden" name="isi" value="{{ old('isi') }}">
+                    <trix-editor input="isi" class="@error('isi') is-invalid @enderror"></trix-editor>
+                    @error('isi')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+
+    <script>
+        const judul = document.querySelector('#judul');
+        const slug = document.querySelector('#slug');
+
+        judul.addEventListener('change', function() {
+
+            fetch('/dashboard/posts/CheckSlug?judul=' + judul.value)
+                .then(Response => Response.json())
+                .then(data => slug.value = data.slug)
+        });
+
+        document.addEventListener('trix-file-accept', function(e) {
+
+            e.preventDefault();
+
+        });
+    </script>
+@endsection
