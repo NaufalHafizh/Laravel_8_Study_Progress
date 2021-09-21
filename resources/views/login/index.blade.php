1d06d98f395f6a1a1556648b31df78a5402984a1
Muhammad Naufal Hafizh <56239834+NaufalHafizh@users.noreply.github.com>
2021-09-21 15:39:39 +0700

    Update index.blade.php

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -9,18 +9,31 @@
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>
             @endif
+            @if (session()->has('LoginGagal'))
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    {{ session('LoginGagal') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
             <main class="form-signin">
-                <form>
+                <form action="/login" method="POST">
+                    @csrf
                     <h1 class="h3 mb-3 fw-normal text-center">LOGIN</h1>
                 
                     <div class="form-floating">
-                        <input type="email" class="form-control mb-3" id="floatingInput" placeholder="name@example.com">
-                        <label class="text-dark" for="floatingInput">Email address</label>
+                        <input type="text" class="form-control mb-3 @error('username') is-invalid @enderror" name="username" id="username" placeholder="username" autofocus required value="{{ old('username') }}">
+                        <label class="text-dark" for="username">Username</label>
                     </div>
+                    @error('username')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
                     <div class="form-floating">
-                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                        <label class="text-dark" for="floatingPassword">Password</label>
+                        <input type="password" class="form-control  @error('password') is-invalid @enderror" name="password" id="password" placeholder="Password">
+                        <label class="text-dark" for="password">Password</label>
                     </div>
+                    @error('password')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
                     <button class="w-100 btn btn-lg btn-primary mt-4 mb-3" type="submit">Sign in</button>
                 </form>
                 <div class="text-center">
