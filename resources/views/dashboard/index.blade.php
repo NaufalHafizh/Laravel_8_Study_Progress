434a8ad58c9794f57b3dbe97b45f139710c2647b
Muhammad Naufal Hafizh <56239834+NaufalHafizh@users.noreply.github.com>
2021-09-21 15:39:22 +0700

    Create index.blade.php

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,12 @@
+@extends('dashboard.layout.main')
+
+@section('container')
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Selamat Datang, {{ auth()->user()->name }}</h1>
+    </div>
+    @if (session()->has('berhasil'))
+        <div class="alert alert-success" role="alert">
+            {{ session('berhasil') }}
+        </div>
+    @endif
+@endsection
